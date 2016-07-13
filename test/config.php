6e678b2b0c8c898324e60a16aab63d1c33534ef8
Alejandro Peña Florentín <alejandropenaflorentin@gmail.com;>
2016-07-13 13:28:07 +0200

    Creados primeros Tests con PHP Unit

---
+++ test/config.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Alejandro Peña Florentín (alejandropenaflorentin@gmail.com).
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+spl_autoload_register(function($class) {
+    $CLASS_DIR = dirname(__DIR__) . "/common/class/";
+    $CLASS_EXT = ".class.php";
+    $CLASS_EXT_2 = ".php";
+    $subdir = str_replace("\\", "/", $class);
+    $file = $CLASS_DIR . $subdir . $CLASS_EXT;
+    $file2 = $CLASS_DIR . $subdir . $CLASS_EXT_2;
+    if (is_readable($file)) {
+        include_once $file;
+    } elseif (is_readable($file2)) {
+        include_once $file2;
+    }
+});
